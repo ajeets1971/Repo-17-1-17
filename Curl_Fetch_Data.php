799c952a0a8e04334eafa60a9ffc10730a34c191
admin\lcom81_two <lanetteam.dipti>
2017-01-17 12:05:37 +0530

    Curl File Added;

---
+++ Curl_Fetch_Data.php
@@ -0,0 +1,26 @@
+<?php
+	$url='http://localhost/repo-17-1-17/Fetch_Product_Data_File.php';
+	$curl=curl_init($url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$curl_response=curl_exec($curl);
+	curl_close($curl);
+	if($curl_response!=false)
+	{
+		$data=json_decode($curl_response);
+		echo "<table border=1><tr>";
+		echo "<td>Product_Name</td><td>	Image</td><td>	Color</td><td>	Size</td><td>	Weight</td><td>	Manufacturer</td><td>	Price</td></tr><tr>";
+		foreach ($data as $key => $value) {
+			foreach ($value as $key => $value) {
+				if($key=="Image")
+				{
+					echo "<td><img src=".$value." height=50 width=50></td>";
+				}
+				else
+				{
+					echo "<td>".$value."</td>";
+				}
+			}
+			echo "</tr>";
+		}
+	}
+?>	
